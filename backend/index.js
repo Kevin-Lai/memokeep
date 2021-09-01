f7d3aa8e8bee5546d1636193b4136bd1b1b32823
Kevin Lai <kevinlai247@gmail.com>
2021-09-01 16:42:10 -0700

    Add note paths to index.js

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,16 @@ const app = express(); // invokes an instance of the 'express' class
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 
+const notes = require('./routes/api/notes');
+
 // connect the web app to mongoDB
 mongoose
     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
+
+app.use(express.json());
+app.use('/api/notes', notes);
 
 // if we go to "/" route, execute the callback function
 // 2 parameters: path, callback
